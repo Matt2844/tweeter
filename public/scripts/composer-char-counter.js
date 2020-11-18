800fcc2e69b3e009d2cc6f9180141835abbd7708
Matt <matt.hd.ford@gmail.com>
2020-11-18 02:58:23 +0000

    finished rough copy of previous tweets section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,20 +4,29 @@ $(document).ready(function() {
   console.log("DOM ready");
 
   $('#tweet-text').keyup(handleKeyUp);
+  $('.previous-tweets-container').mouseenter(tweetNameVisible).mouseleave(tweetNameInvisible);
 })
 
+// Tweet characters left counter       
 const handleKeyUp = function() {
   const length = $(this).val().length;
-  console.log(length);
   const counter = 140;
   const charsLeft = counter - length;
-  $('.counter').text(charsLeft)
+  const charText = $('.counter').text(charsLeft);
+};
 
+// Change tweeter handle to visible on hover
+const tweetNameVisible = function() {
+  $('.hidden-handle').animate({ opacity: 1 }, 100);
+};
+
+// Change tweeter handle opacity back to invsible
+const tweetNameInvisible = function() {
+  $('.hidden-handle').animate({ opacity: 0 }, 100);
 }
 
 
 
-// Text counter
 
 
 
